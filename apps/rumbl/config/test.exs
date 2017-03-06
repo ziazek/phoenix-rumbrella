938c8b22eb328b68ae4c16ec33bb530db6986f9f
Zia Zek <ziazek@gmail.com>
2017-03-06 18:26:20 +0800

    adding rumbl to main repo

---
+++ apps/rumbl/config/test.exs
@@ -0,0 +1,23 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :rumbl, Rumbl.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :rumbl, Rumbl.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "",
+  database: "rumbl_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# Configure comeonin library
+config :comeonin, :bcrypt_log_rounds, 1
+config :comeonin, :pbkdf2_rounds, 1
---
+++ apps/rumbl/web/models/video.ex
@@ -0,0 +1,51 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
+
+  schema "videos" do
+    field :url, :string
+    field :title, :string
+    field :description, :string
+    field :slug, :string
+    belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
+    has_many :annotations, Rumbl.Annotation
+
+    timestamps()
+  end
+
+  @required_fields ~w(url title description)a
+  @optional_fields ~w(category_id)a
+
+  @doc """
+  Builds a changeset based on the `model` and `params`.
+  """
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> slugify_title()
+    |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str 
+    |> String.downcase() 
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+end
+
+defimpl Phoenix.Param, for: Rumbl.Video do
+  def to_param(%{slug: slug, id: id}) do
+    "#{id}-#{slug}"
+  end 
+end
