d04adf2093456c670266942be6d75ad934586244
Zia Zek <ziazek@gmail.com>
2017-03-06 19:46:13 +0800

    (auto message)

--- mix.exs
+++ mix.exs
@@ -3,6 +3,7 @@ defmodule Rumbrella.Mixfile do
 
   def project do
     [apps_path: "apps",
+     version: "0.0.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
