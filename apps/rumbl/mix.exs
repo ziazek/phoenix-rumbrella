af8c4286ae66507a9e9cdf7ec6b117dca5fe6b81
Zia Zek <ziazek@gmail.com>
2017-03-07 10:16:57 +0800

    (auto message)

--- apps/rumbl/mix.exs
+++ apps/rumbl/mix.exs
@@ -3,7 +3,7 @@ defmodule Rumbl.Mixfile do
 
   def project do
     [app: :rumbl,
-     version: append_revision("0.0.4"),
+     version: append_revision("0.0.5"),
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
